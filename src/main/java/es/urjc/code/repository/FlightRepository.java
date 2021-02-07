18b838f503bfc5a68daf08e1e0f01b2f70446105
Dionisio Cortés Fernández <cortesfernandezdionisio@gmail.com>
2021-02-07 16:32:06 +0100

    BEatify queries

--- src/main/java/es/urjc/code/repository/FlightRepository.java
+++ src/main/java/es/urjc/code/repository/FlightRepository.java
@@ -4,16 +4,17 @@ import es.urjc.code.dtos.FlightDto;
 import es.urjc.code.models.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
-    // @Query(value = "select new es.urjc.code.dtos.FlightDto(a.id, f.flightCode, f.airline, f.originAirport, f.destinationAirport, f.departureDate, f.arrivalDate, f.flightDuration) from Flight f join Airport a on a.id = f.destinationAirport.id where a.city = ?1 AND FUNCTION('date_format', f.arrivalDate, '%d-%m-%Y') = ?2 order by f.arrivalDate")
-    // List<FlightDto> findFlightsByCityAndDateOrderedByTime(String city, String date);
-
-    @Query(value = "select new es.urjc.code.dtos.FlightDto(a.id, f.flightCode, f.airline, f.originAirport, f.destinationAirport, f.departureDate, f.arrivalDate, f.flightDuration) from Flight f join Airport a on a.id = f.destinationAirport.id where a.city = ?1 order by f.arrivalDate")
-    List<FlightDto> findFlightsByCityAndDateOrderedByTime(String city);
+    @Query(value = "select new es.urjc.code.dtos.FlightDto(a.id, f.flightCode, f.airline, f.originAirport, f.destinationAirport, f.departureDate, f.arrivalDate, f.flightDuration) " +
+                   "from Flight f join Airport a on a.id = f.destinationAirport.id " +
+                   "where a.city = :city " +
+                   "order by f.arrivalDate")
+    List<FlightDto> findFlightsByCityAndDateOrderedByTime(@Param("city") String city);
 
 }
--- src/main/java/es/urjc/code/repository/TripulantRepository.java
+++ src/main/java/es/urjc/code/repository/TripulantRepository.java
@@ -8,13 +8,18 @@ import org.springframework.data.jpa.repository.Query;
 
 import es.urjc.code.dtos.TripulantDto;
 import es.urjc.code.models.Tripulant;
+import org.springframework.data.repository.query.Param;
 
 public interface TripulantRepository extends JpaRepository<Tripulant, Long> {
 
-    @Query(value = "select new es.urjc.code.dtos.TripulantDto(t.name, t.lastName, a.city, f.departureDate) from Tripulant t join TripulantFlight tf on t.id = tf.tripulant.id join Flight f on f.id = tf.flight.id join Airport a on a.id = f.originAirport.id where t.code = ?1")
-    List<TripulantDto> getTripulantDestinationCitiesAndDatesByTripulantCode(String tripulantCode);
+    @Query(value = "select new es.urjc.code.dtos.TripulantDto(t.name, t.lastName, a.city, f.departureDate) " +
+                   "from Tripulant t join TripulantFlight tf on t.id = tf.tripulant.id join Flight f on f.id = tf.flight.id join Airport a on a.id = f.originAirport.id " +
+                   "where t.code = :tripulantCode")
+    List<TripulantDto> getTripulantDestinationCitiesAndDatesByTripulantCode(@Param("tripulantCode") String tripulantCode);
 
-    @Query(value = "select new es.urjc.code.dtos.TripulantAccumulatedFlightTime(t.name, t.lastName, count(tf), sum(f.flightDuration)) from Tripulant t join TripulantFlight tf on t.id = tf.tripulant.id join Flight f on f.id = tf.flight.id group by t.name, t.lastName")
+    @Query(value = "select new es.urjc.code.dtos.TripulantAccumulatedFlightTime(t.name, t.lastName, count(tf), sum(f.flightDuration)) " +
+                   "from Tripulant t join TripulantFlight tf on t.id = tf.tripulant.id join Flight f on f.id = tf.flight.id " +
+                   "group by t.name, t.lastName")
     List<TripulantAccumulatedFlightTime> getTripulantFlightsAmountAndTotalFlightTime();
 
 }
