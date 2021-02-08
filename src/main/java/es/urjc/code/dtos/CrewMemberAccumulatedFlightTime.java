5b24f2aeac190f5a6e848490e6a6d79b066d593a
Javier García González <javier@garciagon.com>
2021-02-08 19:57:46 +0100

    Beautify query 4

--- src/main/java/es/urjc/code/dtos/CrewMemberAccumulatedFlightTime.java
+++ src/main/java/es/urjc/code/dtos/CrewMemberAccumulatedFlightTime.java
@@ -3,16 +3,25 @@ package es.urjc.code.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class CrewMemberAccumulatedFlightTime {
 
     private String name;
     private String lastName;
     private Long numberOfFlights;
     private Double totalTime;
+
+    @Override
+    public String toString() {
+        return
+            "------Crew Member accumulated flight time-------" +
+                "\n###### Crew Member ######\n" +
+                "\tName='" + name + "'\n" +
+                "\tLast Name='" + lastName + "'\n" +
+                "\tNumber of flights='" + numberOfFlights + "'\n" +
+                "\tAccumulated time='" + totalTime + "'\n";
+    }
 }
